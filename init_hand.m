52d456ceae68939a75903baffba908a3fc72a1ae
Aslakjs <aslakjs@stud.ntnu.no>
2020-12-09 00:10:25 +0100

    added files to repo

---
+++ init_hand.m
@@ -0,0 +1,13 @@
+%% Robotic Hand
+% Author and Copyright: Aslak J. Strand
+% Lisence details:
+%   May be used for anything given the following criterias:
+%       - Author citation
+%           AND
+%       - Non-commersial
+%           OR
+%       - Author(s) are given a financial cut
+
+clearvars; clc; close all;
+
+%%
